e06295989c94c2f71f2ee08999ad3401fde903d0
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-03-26 08:03:49 -1000

    determining round or square watch

--- app/src/main/java/com/example/zappycode/androidweardemo/MainActivity.java
+++ app/src/main/java/com/example/zappycode/androidweardemo/MainActivity.java
@@ -2,11 +2,22 @@ package com.example.zappycode.androidweardemo;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends WearableActivity {
 
     private TextView mTextView;
+
+    public void determineRoundOrSquare(View view){
+        if(getResources().getConfiguration().isScreenRound()){
+            Toast.makeText(this, "ROUND!", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "SQUARE!", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +25,12 @@ public class MainActivity extends WearableActivity {
         setContentView(R.layout.activity_main);
 
         mTextView = (TextView) findViewById(R.id.text);
+
+        if(getResources().getConfiguration().isScreenRound()){
+            Log.i("Device", "Round!");
+        } else {
+            Log.i("Device", "Square!");
+        }
 
         // Enables Always-on
         setAmbientEnabled();
